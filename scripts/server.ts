490cc2fbe220fb52a69e048b7e0ffa2fd0c34e49
Hank Stoever <hstove@gmail.com>
2023-09-07 14:04:14 -0700

    fix: revert server script

--- scripts/server.ts
+++ scripts/server.ts
@@ -1,20 +1,23 @@
 import { api } from '../src';
 import { logger } from '../src/logger';
 
-const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3002;
-logger.info({ port }, 'Starting server');
-const app = await api();
-logger.info('App built, about to listen');
-app.listen(
-  {
+async function run() {
+  const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3002;
+  logger.info({ port }, 'Starting server');
+  const app = await api();
+  logger.info('App built, about to listen');
+  const address = await app.listen({
     host: '0.0.0.0',
     port,
-  },
-  (err, address) => {
-    if (err) {
-      app.log.error(err);
-      process.exit(1);
-    }
-    logger.info({ address }, `Listening at ${address}`);
-  }
-);
+  });
+  logger.info({ address }, `Listening at ${address}`);
+}
+
+run()
+  .catch(error => {
+    logger.error(error);
+    process.exit(1);
+  })
+  .finally(() => {
+    // process.exit();
+  });
